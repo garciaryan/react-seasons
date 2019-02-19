aac68504d6a7b3f3c27b60c21ca725fdcca45afd
Ryan Garcia <garciaryan007@yahoo.com>
2019-02-19 12:36:44 -0800

    adding logic to render text

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9){
+    return lat > 0 ? 'Summer' : 'Winter';
+  }
+  else {
+    return lat > 0 ? 'Winter' : 'Summer';
+  }
+}
+
 const SeasonDisplay = (props) => {
-  return <div>{props.lat}</div>
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text = season === 'Winter' ? 'Brr, it\'s chilly' : 'Let\'s hit the beach';
+
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
 }
 
 export default SeasonDisplay;
